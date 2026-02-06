10e8f1762067383e1d829339cb64061ee7cff00f
mustafaergutt0 <mustafaergutt0@gmail.com>
2026-02-06 11:28:48 +0300

    Strared AlirlinesProject

--- src/main/java/com/ergutlarholding/airlinesproject/AirlinesProjectApplication.java
+++ src/main/java/com/ergutlarholding/airlinesproject/AirlinesProjectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AirlinesProjectApplication {
-
+// Started Ergutlar Airlines proejct
     public static void main(String[] args) {
         SpringApplication.run(AirlinesProjectApplication.class, args);
     }
