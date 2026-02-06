4e186207050db9fe16ef6c1592be581e55219ea0
mustafaergutt0 <kertenkele219@gmail.com>
2026-02-06 11:17:06 +0300

    Airlines Project Started 06/02/2026

---
+++ src/test/java/com/ergutlarholding/airlinesproject/AirlinesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ergutlarholding.airlinesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlinesProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
