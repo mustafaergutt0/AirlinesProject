db98a0ef07db5be70db44521508c44baf23fd55d
mustafaergutt0 <kertenkele219@gmail.com>
2026-02-11 16:35:10 +0300

    That date Euraka service api gateway deployed
    Succesfully roouting  from gateway to pilot or main Service
    
    Some adjustment for routing and euraka service pom xml rearranged

--- airlines-main-service/src/main/java/com/ergutlarholding/airlinesmainservice/Controller/Flight/FlightController.java
+++ airlines-main-service/src/main/java/com/ergutlarholding/airlinesmainservice/Controller/Flight/FlightController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/flights")
+@RequestMapping("/main/flights")
 @RequiredArgsConstructor
 public class FlightController {
 
--- pilot-service/src/main/java/com/ergutlarholding/pilotservice/Controller/PilotController.java
+++ pilot-service/src/main/java/com/ergutlarholding/pilotservice/Controller/PilotController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/pilots")
+@RequestMapping("/pilots")
 @RequiredArgsConstructor
 public class PilotController {
     private final PilotService pilotService;
