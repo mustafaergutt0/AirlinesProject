445665ad6777a30ff8b74d221c6a38f94c39a2c4
mustafaergutt0 <kertenkele219@gmail.com>
2026-02-17 14:25:59 +0300

    LogService Deployed RabbitMq deployed
    New flight' log added different db through RabbitMQ two service is communicate via Rabbit mq

---
+++ log-service/src/test/java/com/ergutlarholding/logservice/LogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ergutlarholding.logservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
